b5bc3a2082fd76f2c1329a372c351e8a7090c983
unknown <Emmanuel@CYB319-PROC-02.cyber.cyberspace.net.ng>
2023-03-09 20:25:34 +0100

    mobile view adjustment 3

--- components/layout/MobileNavigation.js
+++ components/layout/MobileNavigation.js
@@ -5,18 +5,18 @@ import {RiListCheck} from 'react-icons/ri'
  
 const MobileNavigation = () => {
   return (
-    <div className='flex items-center text-[15px] justify-between p-1 bg-slate-100 my-2 mx-4 rounded-sm  md:hidden text-gray-600'>
+    <div className='flex items-center text-[15px] justify-between p-1 bg-slate-100 my-2 mx-2 rounded-sm  md:hidden text-gray-600'>
 
       <Link href='/' className='flex gap-2 items-center font-semibold cursor-pointer w-full mx-[2px] py-[2px] px-2'>
-      <RxDashboard  className='' /><h3>Dashboard</h3>
+      <RxDashboard  className='' /><h3 className='text-[14px]'>Dashboard</h3>
       </Link>
       <div className='h-6 w-2 bg-white'></div>
-      <Link href='/my-projects' className='flex gap-3 items-center font-semibold cursor-pointer w-full mx-[2px] py-[2px] px-2'>
-      <CgList className='' /> <h3>My Projects</h3>
+      <Link href='/my-projects' className='flex gap-2 items-center font-semibold cursor-pointer w-full mx-[2px] py-[2px] px-2'>
+      <CgList className='' /> <h3 className='text-[14px]'>My Projects</h3>
       </Link>
       <div className='h-6 w-2 bg-white'></div>
-      <Link href='/my-tasks' className=' w-full flex gap-3 items-center font-semibold cursor-pointer mx-[2px] py-[2px] px-2'>
-      <RiListCheck className='' /> <h3>My Tasks</h3>
+      <Link href='/my-tasks' className=' w-full flex gap-2 items-center font-semibold cursor-pointer mx-[2px] py-[2px] px-2'>
+      <RiListCheck className='' /> <h3 className='text-[14px]'>My Tasks</h3>
       </Link>
     </div>
   )
