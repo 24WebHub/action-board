c4a39b4ba73719d8e5e0fe42d78716e297c66b63
unknown <Emmanuel@CYB319-PROC-02.cyber.cyberspace.net.ng>
2023-03-09 20:32:27 +0100

    adjustment to mobile view 3

--- components/layout/MobileNavigation.js
+++ components/layout/MobileNavigation.js
@@ -5,7 +5,7 @@ import {RiListCheck} from 'react-icons/ri'
  
 const MobileNavigation = () => {
   return (
-    <div className='flex items-center text-[15px] justify-between py-2 bg-slate-100 my-2 mx-2 rounded-sm  md:hidden text-gray-600'>
+    <div className='flex items-center text-[15px] justify-between py-2 bg-slate-100 my-2 mx-4 rounded-sm  md:hidden text-gray-600'>
 
       <Link href='/' className='flex gap-2 items-center font-semibold cursor-pointer w-full mx-[2px] py-[2px] px-2'>
       <RxDashboard  className='' /><h3 className='text-[14px]'>Dashboard</h3>
